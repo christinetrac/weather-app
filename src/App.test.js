0ad8e9a77ce61eb64265092ff475c7c07cd347a4
christinetrac <christineetrac@gmail.com>
2021-10-06 15:51:08 -0400

    unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders copyright', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Developed and designed by Christine Trac for On Deck/i);
   expect(linkElement).toBeInTheDocument();
 });
