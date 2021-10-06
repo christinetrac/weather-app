eee6711c4b56d3a3960fdfaf1ff78fe6d7ce4d96
christinetrac <christineetrac@gmail.com>
2021-10-05 23:58:24 -0400

    weather app!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,132 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
+import config from './config';
+import vector1 from "./assets/vector1.svg";
+import vector2 from "./assets/vector2.svg";
+import vector3 from "./assets/vector3.svg";
+import Select from 'react-select';
+import MaterialIcon from 'material-icons-react';
 
 function App() {
-  return (
+    const [currentCityData, setCurrentCityData] = useState({});
+
+    const fetchData = (cityValue) => {
+        fetch(`https://api.weatherbit.io/v2.0/current?city=${cityValue}&key=${config.api.key}`)
+            .then((response) => response.json())
+            .then((json) => setCurrentCityData(json.data[0]))
+            .catch((error) => console.error(error));
+    };
+
+    const handleCitySelection = (city) => {
+        fetchData(city.value);
+    };
+
+    useEffect(() => {
+        fetchData('toronto&country=canada');
+    }, []);
+
+    const options = [
+        { value: 'toronto&country=canada', label: 'Toronto' },
+        { value: 'newyork', label: 'New York' },
+        { value: 'losangeles', label: 'Los Angeles' },
+        { value: 'chicago', label: 'Chicago' },
+        { value: 'phoenix', label: 'Phoenix' },
+        { value: 'vancouver&country=canada', label: 'Vancouver' },
+        { value: 'seattle', label: 'Seattle' },
+        { value: 'boston', label: 'Boston' },
+        { value: 'bangkok', label: 'Bangkok' },
+        { value: 'paris', label: 'Paris' },
+        { value: 'tokyo', label: 'Tokyo' },
+        { value: 'shanghai', label: 'Shanghai' },
+        { value: 'dubai', label: 'Dubai' },
+    ];
+
+    const customStyles = {
+        option: (provided, state) => ({
+            ...provided,
+            paddingLeft: 30,
+            color: '#4E4E4F',
+        }),
+        control: () => ({
+            background: '#FEF2FF',
+            borderRadius: 100,
+            height: 62,
+            width: 380,
+            color: '#4E4E4F',
+        }),
+        container: (provided) => ({
+            ...provided,
+            position: 'absolute',
+            left: 92,
+            top: 28,
+            fontFamily: ["Manrope", "sans-serif"].join(","),
+            fontSize: 35,
+            fontWeight: 700,
+            color: '#4E4E4F',
+            textAlign: 'left',
+        }),
+        valueContainer: (provided) => ({
+            ...provided,
+            paddingLeft: 30,
+            color: '#4E4E4F',
+        }),
+        dropdownIndicator: (provided, state) => ({
+            display: 'none'
+        }),
+    };
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={vector1} alt="" className="vector1"/>
+      <img src={vector2} alt="" className="vector2"/>
+      <img src={vector3} alt="" className="vector3"/>
+      <div className="copyright">Developed and designed by Christine Trac for On Deck</div>
+      <div className="container">
+          <div className="tab">
+              <div className="locationIcon">
+                  <MaterialIcon icon="location_on" size={45} color='#4E4E4F' />
+              </div>
+              <Select
+                  className="basic-single"
+                  classNamePrefix="select"
+                  defaultValue={options[0]}
+                  isClearable={false}
+                  isSearchable={true}
+                  name="color"
+                  options={options}
+                  styles={customStyles}
+                  theme={(theme) => ({
+                      ...theme,
+                      colors: {
+                          ...theme.colors,
+                          text: '#4E4E4F',
+                          primary25: '#FEF2FF',
+                          primary: '#9E9FFF',
+                      },
+                  })}
+                  onChange={(e) => handleCitySelection(e)}
+              />
+          </div>
+          <div className="weatherCard">
+            <div className="circle">
+                <img src={`/icons/${currentCityData?.weather?.icon}.png`} className="weatherIcon" alt=""/>
+            </div>
+            <div className="weather">
+                <div className="currentWeather">
+                    {Math.round(currentCityData?.temp)}
+                    <span> °C</span>
+                </div>
+                <div className="apparentWeather">
+                    and feels like {Math.round(currentCityData?.app_temp)}
+                    <span> °C</span>
+                </div>
+            </div>
+            <div className="weatherDescription">
+                {currentCityData?.weather?.description}
+            </div>
+          </div>
+          <div className="ruler"/>
+      </div>
     </div>
   );
 }
